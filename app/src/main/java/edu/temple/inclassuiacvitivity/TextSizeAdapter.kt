b9f694da2b8f49a735a39e456240960f16d95b47
NateScott28 <tul07731@temple.edu>
2024-02-08 11:46:39 -0500

    Implemented getView() to show each font as its respective font

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 
 class TextSizeAdapter(context: Context, fonts: Array<int>) : BaseAdapter() {
 
@@ -23,7 +24,10 @@ class TextSizeAdapter(context: Context, fonts: Array<int>) : BaseAdapter() {
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+        val textView = TextView(context);
+        textView.textSize = fontSizes[position];
+        textView.setPadding(5, 10, 0, 10);
+        return textView;
     }
 
 }
